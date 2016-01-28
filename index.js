b5fc8dc78b369bfb5ffd2ec541e1c482c2d54a80
Tony Spaskyi <jim.casey321@gmail.com>
2016-01-28 22:18:48 +0200

    style change

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ $(document).ready(function() {
 		if($(window).height > 720) {
 			landing.height($(window).height());
 		}
+
+		if($(window).width() < 1030) {
+
+		}
 	});
 
 	function slider() {
@@ -70,12 +74,14 @@ $(document).ready(function() {
 
 		function moveElements() {
 			$containerForImg.children().remove();
-			$(".about-us__carousel-buttons-item" + valueNumberSliderItem).removeClass("about-us__carousel-buttons-item_active");
+			$(".about-us__carousel-buttons-item" + valueNumberSliderItem)
+				.removeClass("about-us__carousel-buttons-item_active");
 			if (valueNumberSliderItem == arrValue.length - 1) {
 				valueNumberSliderItem = -1;
 			}
 			valueNumberSliderItem++;
-			$(".about-us__carousel-buttons-item" + valueNumberSliderItem).addClass("about-us__carousel-buttons-item_active");
+			$(".about-us__carousel-buttons-item" + valueNumberSliderItem)
+				.addClass("about-us__carousel-buttons-item_active");
 			$containerForImg.append(arrValue[valueNumberSliderItem]);
 		}
 
@@ -87,10 +93,12 @@ $(document).ready(function() {
 			}
 			clearInterval(timer);
 			$containerForImg.children().remove();
-			$(".about-us__carousel-buttons-item" + valueNumberSliderItem).removeClass("about-us__carousel-buttons-item_active");
+			$(".about-us__carousel-buttons-item" + valueNumberSliderItem)
+				.removeClass("about-us__carousel-buttons-item_active");
 			numberImage = className.substring(63);
 			valueNumberSliderItem = numberImage;
-			$(".about-us__carousel-buttons-item" + valueNumberSliderItem).addClass("about-us__carousel-buttons-item_active");
+			$(".about-us__carousel-buttons-item" + valueNumberSliderItem)
+				.addClass("about-us__carousel-buttons-item_active");
 			$containerForImg.append(arrValue[valueNumberSliderItem]);
 			timer = setInterval(moveElements, timeForTimerMs);
 		}
