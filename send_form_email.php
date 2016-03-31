af71715cf783030e352ed1f34728c2c28dd7cff0
Tony Spaskyi <jim.casey321@gmail.com>
2016-04-01 00:03:10 +0300

    Add logic for sending form data to the email

--- send_form_email.php
+++ send_form_email.php
@@ -1,8 +1,20 @@
 <?php
 
-if(!isset($_POST['submit'])) {
+if(!isset($_POST['name'])) {
     echo "error ";
 }
 
 $name = $_POST['name'];
-var_dump($_POST);
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$to = "jim.casey321@gmail.com";
+
+$headers = "From: $name \r\n";
+$headers .= "Reply-To: $email \r\n";
+
+$email_subject = "New customer mail!";
+$email_body = $name . " $phone " . " $email " . " $message";
+
+mail($to, $email_subject, $email_body, $headers);
